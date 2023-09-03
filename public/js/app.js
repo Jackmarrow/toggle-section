73fd12b4f1d84bbcb7e3b4c14f3fab94552ff262
Oussama Bof <jackmarrow06@gmail.com>
2023-09-04 00:01:02 +0200

    section drop feature added

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,10 @@ const allBtn = document.querySelectorAll('.open-modal');
 allBtn.forEach(element => {
     element.addEventListener('click', ()=>{
         allModal.forEach(ele => {
-            if(element.getAttribute('id') === ele.getAttribute('id')){
+            if(ele.className.includes('show') && element.getAttribute('id') !== ele.getAttribute('id')){
+                ele.classList.remove('show');
+            }
+            else if(element.getAttribute('id') === ele.getAttribute('id')){
                 ele.classList.toggle('show')
             }
         })
